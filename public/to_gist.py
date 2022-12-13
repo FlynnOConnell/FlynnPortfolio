fd718e81ad60f2e1c181dbbef406fb1a4f9cf910
Flynn <75346097+NeuroPyPy@users.noreply.github.com>
2022-12-13 12:31:14 -0500

    Update to_gist.py

--- public/to_gist.py
+++ public/to_gist.py
@@ -5,7 +5,7 @@ embed github gists in vue components.
 
 In your terminal, run:
 
-python <(curl https://raw.githubusercontent.com/NeuroPyPy/FlynnPortfolio/master/public/to_gist.py) <USERNAME>/<GIST_ID/(optional)FILENAME>
+python <(curl https://raw.githubusercontent.com/NeuroPyPy/FlynnPortfolio/master/public/to_gist.py) <USERNAME>/<GIST_ID COMPONENT_NAME>
 """
 import argparse
 from bs4 import BeautifulSoup as bs
@@ -16,36 +16,12 @@ except ImportError:
     from urllib2 import urlopen
 import json
 
-parser = argparse.ArgumentParser()
-parser.parse_args()
+gist_url = f"https://gist.github.com/{sys.argv[1]}.json"
 
-parser.add_argument("username", type=str, help="Place your github username here.")
-parser.add_argument("gistnumber", type=str, help="The ID of the gist youd like to embed. "
-                                             "Find this by visiting the browser page "
-                                             "containing your gist: "
-                                             "<https://gist.github.com/GithubUsernameHere/[Long string of numbers and letters]>"
-                                             "Here you want the [Long string] without "
-                                             "the brackets or < >.")
-
-parser.add_argument("--filename", type=str, help="(Optional) What to name the file. "
-                                                 "Default is: Gist")
-
-args = parser.parse_args()
-
-username = args.username
-gistnumber = args.gistnumber
-
-if args.filename:
-    filename = args.filename
-else:
-    filename = "Gist"
-
-gist_url = f"https://gist.github.com/{username}/{gistnumber}.json"
 gist = json.loads(urlopen(gist_url).read())
 html = bs(gist['div'], 'html.parser').prettify()
 
-stylesheeturl = urlopen("https://raw.githubusercontent.com/NeuroPyPy/FlynnPortfolio"
-                      "/master/public/cobalt_gist.css").read()
+stylesheeturl = urlopen("https://raw.githubusercontent.com/NeuroPyPy/FlynnPortfolio/master/public/cobalt_gist.css").read()
 stylesheet = bs(stylesheeturl, 'html.parser').prettify()
 
 template = bs(f"<template>{html}</template>", 'html.parser').prettify()
