54a4d82d4fbe96d0252cf5fd19e4632389941073
Flynn <75346097+NeuroPyPy@users.noreply.github.com>
2022-12-13 12:20:21 -0500

    Update to_gist.py

--- public/to_gist.py
+++ public/to_gist.py
@@ -1,20 +1,56 @@
-# python <(curl https://raw.githubusercontent.com/NeuroPyPy/FlynnPortfolio/master/public/to_gist.py) <USERNAME>/<GIST_ID>
+
+"""
+Adaptation of https://nickwu241.medium.com/embed-a-gist-statically-in-vue-4df59b56b29 to 
+embed github gists in vue components.
+
+In your terminal, run:
+
+python <(curl https://raw.githubusercontent.com/NeuroPyPy/FlynnPortfolio/master/public/to_gist.py) <USERNAME>/<GIST_ID/(optional)FILENAME>
+"""
+import argparse
+from bs4 import BeautifulSoup as bs
+
 try:
     from urllib.request import urlopen
 except ImportError:
     from urllib2 import urlopen
 import json
-import sys
 
-# Modify COMPONENT_NAME to name your component differently.
-COMPONENT_NAME = "Gist"
+parser = argparse.ArgumentParser()
+parser.parse_args()
 
-gist_url = "https://gist.github.com/{:s}.json".format(sys.argv[1])
+parser.add_argument("username", type=str, help="Place your github username here.")
+parser.add_argument("gistnumber", type=str, help="The ID of the gist youd like to embed. "
+                                             "Find this by visiting the browser page "
+                                             "containing your gist: "
+                                             "<https://gist.github.com/GithubUsernameHere/[Long string of numbers and letters]>"
+                                             "Here you want the [Long string] without "
+                                             "the brackets or < >.")
+
+parser.add_argument("--filename", type=str, help="(Optional) What to name the file. "
+                                                 "Default is: Gist")
+
+args = parser.parse_args()
+
+username = args.username
+gistnumber = args.gistnumber
+
+if args.filename:
+    filename = args.filename
+else:
+    filename = "Gist"
+
+gist_url = f"https://gist.github.com/{username}/{gistnumber}.json"
 gist = json.loads(urlopen(gist_url).read())
-html = gist["div"]
-stylesheet = urlopen(gist["stylesheet"]).read()
+html = bs(gist['div'], 'html.parser').prettify()
 
-with open("{:s}.vue".format(COMPONENT_NAME), "w") as f:
-    f.write("<template>{:s}</template>".format(html))
-    f.write("<script>export default {{ name: '{:s}' }}</script>".format(COMPONENT_NAME))
-    f.write("<style>{:s}</style>".format(stylesheet))
+stylesheeturl = urlopen("https://raw.githubusercontent.com/NeuroPyPy/FlynnPortfolio"
+                      "/master/public/cobalt_gist.css").read()
+stylesheet = bs(stylesheeturl, 'html.parser').prettify()
+
+template = bs(f"<template>{html}</template>", 'html.parser').prettify()
+
+with open(f"{filename}.vue", "w") as f:
+    f.write(template)
+    f.write(f"<script>export default {{ name: '{filename}' }}</script>")
+    f.write(f"<style>{stylesheet}</style>")
